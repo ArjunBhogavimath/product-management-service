df6936fc2c06e18b4f5dc927699d385f07986484
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2025-04-12 18:15:22 +0530

    Implemented all endpoints in controller layer

---
+++ src/main/java/com/edstruments/product_service/contoller/ProductController.java
@@ -0,0 +1,55 @@
+package com.edstruments.product_service.contoller;
+
+import com.edstruments.product_service.entity.Product;
+import com.edstruments.product_service.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts(){
+        List<Product> products = productService.getAll();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id){
+        Product product = productService.getProductById(id);
+        return (product == null) ? ResponseEntity.notFound().build() : ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+        Product createdProduct = productService.createProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product, @PathVariable Long id){
+        Product updatedProduct = productService.updateProductById(product, id);
+        return (updatedProduct == null) ?
+                ResponseEntity.notFound().build() :
+                ResponseEntity.ok(updatedProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id){
+        Product existingProduct = productService.getProductById(id);
+        if(existingProduct != null){
+            productService.deleteProduct(id);
+            return ResponseEntity.ok("Product deleted successfully");
+        } else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+    }
+}
