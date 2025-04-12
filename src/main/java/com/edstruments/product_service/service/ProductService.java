4d926dabee653e7b06fe83cf9585986e138ddf87
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2025-04-12 18:16:11 +0530

    Implemented service layer logic for controller layer endpoints

---
+++ src/main/java/com/edstruments/product_service/service/ProductService.java
@@ -0,0 +1,43 @@
+package com.edstruments.product_service.service;
+
+import com.edstruments.product_service.entity.Product;
+import com.edstruments.product_service.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll(){
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id){
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product createProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public Product updateProductById(Product product, Long id){
+        Product existingProduct = productRepository.findById(id).orElse(null);
+        if(existingProduct != null){
+            existingProduct.setName(product.getName());
+            existingProduct.setDescription(product.getDescription());
+            existingProduct.setPrice(product.getPrice());
+            return productRepository.save(existingProduct);
+        }
+        return null;
+
+    }
+
+    public void deleteProduct(Long id){
+        productRepository.deleteById(id);
+    }
+}
